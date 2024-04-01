bde095b271696697139fd346ecacd85a586d2b9d
joaovcostas <jvc22102003@gmail.com>
2024-04-01 11:35:37 -0300

    feat: print messagen on -listen- method

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,10 @@
 import { app } from './app'
 
-app.listen({
-  host: '0.0.0.0',
-  port: 3333,
-})
+app
+  .listen({
+    host: '0.0.0.0',
+    port: 3333,
+  })
+  .then(() => {
+    console.log('🔥 HTTP Server running')
+  })
